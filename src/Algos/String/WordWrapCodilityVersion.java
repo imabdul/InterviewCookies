39bcccbe92287cc218bb5a76883e4438802afbf4
Abdul Malik <abdul.malik@kohls.com>
2019-10-22 11:43:10 -0700

    commenting and logic improved for wordWrap

--- src/Algos/String/WordWrapCodilityVersion.java
+++ src/Algos/String/WordWrapCodilityVersion.java
@@ -24,28 +24,52 @@ package Algos.String;
  */
 
 public class WordWrapCodilityVersion {
-    public static String getWordWrap(String s, int k) {
+    public String getWordWrap(String s, int k) {
+        /**
+         * when k is bigger than string s length itself return the whole string, simple.
+         */
+        if(k>s.length()) return s;
+
+        /**
+         * break down s into words array
+         */
         String[] words = s.split(" ");
         int index=0;
         int len = words.length;
-        if(len<=k) return s;
         int count=0;
         //StringBuilder sb = new StringBuilder();
-        while(index < len && count+words[index].length()+1<=k){
+        //System.out.println(count);
+
+        /**
+         *count (prev value of count) + next word length + 1(space after word) should be smaller or equal to k
+         */
+        while(count+words[index].length()+1<=k){
+            //System.out.println(count);
             count+=words[index].length()+1;
+            /**
+             * if the next word can't be accommodated within k, remove space from the end.
+             */
+            if (count+words[index+1].length()>k) count--;
+            /**
+             * edge case coverage, if the next word is last word and exactly fitting within k, add it to the count. this is where loop is goign to end.
+             */
+            if (count+words[index+1].length()==k) count+=words[index+1].length();
+
             index++;
         }
-        return count==0?new String(""):s.substring(0,count);
+        //System.out.println(count);
+        return count==0? new String(""):s.substring(0,count);
     }
 
     public static void main(String[] args) {
+        WordWrapCodilityVersion ww = new WordWrapCodilityVersion();
         String s1 = "Codility We test coders"; int k1 = 14;
         String s2 = "The quick brown fox jumps over the lazy dog"; int k2 = 39;
         String s3 = "Why not"; int k3 = 39;
         String s4 = "Codility We test coders"; int k4 = 2;
-        System.out.println(getWordWrap(s1, k1));
-        System.out.println(getWordWrap(s2, k2));
-        System.out.println(getWordWrap(s3, k3));
-        System.out.println(getWordWrap(s4, k4));
+        System.out.println(ww.getWordWrap(s1, k1));
+        System.out.println(ww.getWordWrap(s2, k2));
+        System.out.println(ww.getWordWrap(s3, k3));
+        System.out.println(ww.getWordWrap(s4, k4));
     }
 }
