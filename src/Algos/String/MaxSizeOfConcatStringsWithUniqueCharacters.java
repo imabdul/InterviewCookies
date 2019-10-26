aa836d94583a890536243517e02136f38a2903fa
Abdul Malik <abdul.malik@kohls.com>
2019-10-26 11:57:06 -0700

    HashMap is required for this problem

--- src/Algos/String/MaxSizeOfConcatStringsWithUniqueCharacters.java
+++ src/Algos/String/MaxSizeOfConcatStringsWithUniqueCharacters.java
@@ -23,19 +23,20 @@ import java.util.*;
  *      2. each string in A consists of lowercase English letters;
  *      3. the sum of lengths of strings in A doesn't exceed 100;
  *
+ *
+ *
  */
 public class MaxSizeOfConcatStringsWithUniqueCharacters {
     public int maxSize(String[] A) {
-        Map<Integer, Integer> map = new HashMap<>(); // can be removed (not necessary)
+        Map<Integer, Integer> map = new HashMap<>();
         int res=0;
-
 
         Arrays.sort(A, (a, b) -> Integer.compare(b.length(), a.length()));
 
         for (int t = 0; t < A.length; t++) {
             char[] arr1 = A[t].toCharArray();
             Set<Character> set = new HashSet<>();
-            for (int k = 0; k < arr1.length; k++) { // loop through each word
+            for (int k = 0; k < arr1.length; k++) {
                 if (set.contains(arr1[k])) {
                     break;
                 }
@@ -46,46 +47,41 @@ public class MaxSizeOfConcatStringsWithUniqueCharacters {
         }
         System.out.println(map.values());
 
-        //for (String s : A) System.out.println(s);
 
 
-
-        for(int i=0; i<A.length;i++) { //loop through entire Array
-            if (map.get(i) == 0) continue; //itya //ignored
+        for(int i=0; i<A.length;i++) {
+            if (map.get(i) == 0) continue;
 
             int len = 0;
             char[] arr1 = A[i].toCharArray();
+            //System.out.println("String1 " + A[i]);
             Set<Character> set = new HashSet<>();
-            for (int k = 0; k < arr1.length; k++) { // loop through each word
+            for (int k = 0; k < arr1.length; k++) {
                 if (set.contains(arr1[k])) {
                     break;
                 }
-                set.add(arr1[k]); //itya in set
+                set.add(arr1[k]);
             }
-            if (arr1.length != set.size()) continue;  //ignored
+            if (arr1.length != set.size()) continue;
             else {
-                len += arr1.length; //2
-                System.out.println(Arrays.asList(set));
+                len += arr1.length;
             }
-            //System.out.println(set.contains('i'));
+
             for (int j = 0; j < A.length; j++) {
-                if (j==i || map.get(j) == 0) continue; //ignored
+                if (j==i ) continue; //ignored
+                if(map.get(j) == 0) continue;
                 char[] arr2 = A[j].toCharArray();
+                //System.out.println("String2 " + A[j]);
                 for (int k = 0; k < arr2.length; k++) {
                     if (set.contains(arr2[k])) {
                         break;
                     }
                     set.add(arr2[k]);
                 }
-                //for(char c: set) System.out.println(c);
-               /* System.out.println("arr2len -->" + arr2.length);
-                System.out.println("len -->" + len);
-                System.out.println("setSize -->" + set.size());*/
+
                 if (len + arr2.length != set.size()) break;
                 else {
-                    //System.out.println("hello");
                     len += arr2.length;
-                    System.out.println(Arrays.asList(set));
                 }
             }
             res=Math.max(len,res);
@@ -95,14 +91,14 @@ public class MaxSizeOfConcatStringsWithUniqueCharacters {
 
     public static void main (String[] args){
         MaxSizeOfConcatStringsWithUniqueCharacters concatStrings = new MaxSizeOfConcatStringsWithUniqueCharacters();
-        String[] str1 = new String[]{"co","dil","ity"};
+        String[] str1 = new String[]{"co","dil","itya"};
         System.out.println(concatStrings.maxSize(str1));//expected 5
         String[] str2 = new String[]{"abc","kkk","def","csv"};
         System.out.println(concatStrings.maxSize(str2));//expected 6
         String[] str3 = new String[]{"potato","kayak","banana","racecar"};
         System.out.println(concatStrings.maxSize(str3));//expected 0
         String[] str4 = new String[]{"eva","jwq","tyn","jan"};
-        System.out.println(concatStrings.maxSize(str4));//expected 9
+        System.out.println(concatStrings.maxSize(str4));//expected 90
     }
 
 }
