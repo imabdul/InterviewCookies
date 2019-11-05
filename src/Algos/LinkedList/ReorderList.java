82b881abb7450cf36a5300eb0bf4e0fd246352f8
Abdul Malik <abdul.malik@kohls.com>
2019-11-05 10:12:36 -0800

    commenting

--- src/Algos/LinkedList/ReorderList.java
+++ src/Algos/LinkedList/ReorderList.java
@@ -1,10 +1,22 @@
 package Algos.LinkedList;
 
-/*
-Idea:
-• Bisect the list
-• Reverse the second half
-• Join the two semi-lists in intertwining fashion with head of first list being the head of the joined list.
+/**
+ * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+ * reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ * Example 1:
+ *
+ * Given 1->2->3->4, reorder it to 1->4->2->3.
+ * Example 2:
+ *
+ * Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+ *
+ * Idea:
+ * • Bisect the list
+ * • Reverse the second half
+ * • Join the two semi-lists in intertwining fashion with head of first list being the head of the joined list.
 */
 
 import java.util.List;
