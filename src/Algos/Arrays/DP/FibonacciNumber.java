86eafea745100efa0f52475d4419d998821eb84e
Abdul Malik <abdul.malik@kohls.com>
2020-01-07 15:49:31 -0800

    commenting

--- src/Algos/Arrays/DP/FibonacciNumber.java
+++ src/Algos/Arrays/DP/FibonacciNumber.java
@@ -15,8 +15,7 @@ public class FibonacciNumber {
         dp[1]=1;
 
         for(int i=2; i<=n; i++){
-            /* Add the previous 2 numbers in the series
-         and store it */
+            /* Add the previous 2 numbers in the series and store it */
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
