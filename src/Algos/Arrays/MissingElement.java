9bb3a2db8431c6e15e32669d0136bbbf24754ba2
abdulmalik <malk.abdul@gmail.com>
2020-05-12 09:00:42 -0700

    Array problem added

---
+++ src/Algos/Arrays/MissingElement.java
@@ -0,0 +1,19 @@
+package Algos.Arrays;
+
+public class MissingElement {
+    public int missingNumber(int[] nums) {
+        int missing= nums.length;
+        for(int i=0; i<nums.length;i++){
+            missing ^= i^nums[i];
+        }
+        return missing;
+    }
+    public static void main(String[ ] args){
+        MissingElement me = new MissingElement();
+        int[] test1 = new int[]{3,0,1};
+        System.out.println(me.missingNumber(test1)); //expected 2
+
+        int[] test2 = new int[]{9,6,4,2,3,5,7,0,1};
+        System.out.println(me.missingNumber(test2)); //expected 8
+    }
+}
